d7e95695827445eb4602aec5922a6879ee685c95
Mada <mm.bica@yahoo.ro>
2019-08-06 17:26:05 +0300

    Code refactor + Tested app in Chrome and Firefox for responsive layout

--- resources/views/Authentification/login.blade.php
+++ resources/views/Authentification/login.blade.php
@@ -3,9 +3,9 @@
 @section('title','Login')
 @section('body-id','login-body')
 
+<!-- Left side of the login page - with form -->
 @section('login-left-side')
-
-    <form class="login-form" style="border:1px solid #FFFFEF">
+    <form class="login-form d-flex justify-content-center align-items-center" style="">
         <div class="row form-container text-center justify-content-center" style="">
             <div class="col-12 form-group justify-content-center align-items-center" >
                 <h4 class="form-title">log in</h4>
@@ -20,39 +20,43 @@
                 <button class="btn btn-primary w-100 login-button"> Log in </button>
             </div>
         </div>
-
-
+        <a href="#" class="forgot-password">Forgot password</a>
     </form>
 
 @stop
-
+<!-- Center part of the login page - where is the 'or' text -->
 @section('login-center-side')
-    <span class="text-container" style="margin:15px 0;">
+    <span class="text-container">
         <span class="o-color">o</span>
         <span class="r-color">r</span>
     </span>
 @stop
 
+<!-- Right part of the login page - the one with the 'Create a new account' button -->
 @section('login-right-side')
-    <a href="#" class="btn btn-primary sign-up-button d-flex align-items-center" style="border:1px solid #8CC5DE;text-transform:uppercase; text-decoration:none; padding:15px;font-size:18px; margin:15px 0;background-color: #FFFFEF;color: #4E759C;font-weight:bold;height:60px;">
+    <a href="#" class="btn btn-primary sign-up-button d-flex align-items-center">
         Create a new account
     </a>
 @stop
-@section('content')
-    <div class="container-fluid d-flex justify-content-center align-items-center" style="position:relative">
-        <div class="col-12" style="position:absolute; top:0;">
-            <div class="row m-0 ">
-                <div class="col-12 col-sm-5 d-flex justify-content-center align-items-center" style="color:#4E759C; padding:10px; " >
-                    <h6>Welcome</h6>
-                </div>
-                <div class="col-12 col-sm-2 d-none d-sm-flex">
-                </div>
-                <div class="col-12 col-sm-5 d-none d-sm-flex justify-content-center align-items-center" style="color:#FFFFEF;padding:10px; ">
-                    <h6>New member ?</h6>
-                </div>
-            </div>
-        </div>
 
+@section('login-header')
+    <div class="row m-0 ">
+        <div class="col-12 col-sm-5 d-flex justify-content-center align-items-center header-text header-left-text">
+            <h6>Welcome</h6>
+        </div>
+        <div class="col-12 col-sm-2 d-none d-sm-flex">
+        </div>
+        <div class="col-12 col-sm-5 d-none d-sm-flex justify-content-center align-items-center header-text header-right-text">
+            <h6>New member ?</h6>
+        </div>
+    </div>
+
+@stop
+@section('content')
+    <div class="container-fluid d-flex justify-content-center align-items-center">
+        <div class="col-12 header-container" >
+            @yield('login-header')
+        </div>
         <div class="row m-0 w-100 h-100">
             <div class="left-side col-12 col-sm-5 d-flex justify-content-center align-items-center">
                 @yield('login-left-side')
